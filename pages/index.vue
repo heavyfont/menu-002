dc7d15176da32e52951815a7bea9e25e4175452c
vicky <vickywitvicky@gmail.com>
2024-03-19 23:56:50 +0800

    uoploaded new image

--- pages/index.vue
+++ pages/index.vue
@@ -18,7 +18,7 @@
  <section class="h-screen flex flex-col justify-center">
     <div class="relative h-full">
       <figure class="absolute left-0 top-0 w-full h-full">
-        <NuxtImg  class="w-full h-full object-cover" src="https://res.cloudinary.com/dnwakycvo/image/upload/v1710860213/sample/neom-9633dHhioC8-unsplash_zz1jjd.jpg" alt="" />
+        <NuxtImg  class="w-full h-full object-cover" src="https://res.cloudinary.com/dnwakycvo/image/upload/v1710863765/sample/neom-9633dHhioC8-unsplash_n5dzuf.jpg" alt="" />
       </figure>
       <div class="absolute inset-0 flex items-center justify-center">
         <h1 class="lg:text-8xl text-5xl text-white text-center flex flex-col">
